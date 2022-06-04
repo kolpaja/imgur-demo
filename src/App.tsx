fc1207111e00372fc9ad5bcdf87616bc325dc557
Sokol Paja <kol.paja@gmail.com>
2022-06-04 06:00:24 +0200

    add basic structure with redux/toolkit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Counter from './components/counter';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Imgur image gallery</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
