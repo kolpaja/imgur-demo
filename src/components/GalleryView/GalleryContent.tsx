4c546d8b929365b99ba9b9d88add39c1229457f3
Sokol Paja <kolpaja28@gmail.com>
2022-07-05 11:45:58 +0100

    restarting the project

--- src/components/GalleryView/GalleryContent.tsx
+++ src/components/GalleryView/GalleryContent.tsx
@@ -7,35 +7,55 @@ type GalleryBodyProps = {
 const GalleryContent = ({ gallery }: GalleryBodyProps) => {
   return (
     <div className='gallery-content'>
-      <h1 className='title'>{gallery.title}</h1>
-      <p className='name'>
-        by <strong>{gallery.account_url}</strong>
-      </p>
-      {gallery.images ? (
-        gallery.images.map((image: any) => {
-          return (
-            <div key={image.id}>
-              <div className='image-wrapper'>
-                {(gallery.images && gallery.images[0].animated && (
-                  <video preload='auto' autoPlay loop className='image'>
-                    <source src={gallery.images[0].mp4} type='video/mp4' />
-                  </video>
-                )) ||
-                  (gallery.image && (
-                    <img src={gallery.image.link} alt={image.title} />
-                  )) || (
-                    <img src={image.link} alt={image.title} className='image' />
-                  )}
+      <section className='content-header'>
+        <h1 className='title'>{gallery.title}</h1>
+        <p className='name'>
+          by <strong>{gallery.account_url}</strong>
+        </p>
+      </section>
+      <div className='content-wrapper'>
+        {gallery.images ? (
+          gallery.images.map((image: any) => {
+            return (
+              <div key={image.id}>
+                <div className='media-container'>
+                  {(gallery.images && gallery.images[0].animated && (
+                    <div className='image-container'>
+                      <video preload='auto' autoPlay loop className='image'>
+                        <source src={gallery.images[0].mp4} type='video/mp4' />
+                      </video>
+                    </div>
+                  )) ||
+                    (gallery.image && (
+                      <div className='image-container'>
+                        <img src={gallery.image.link} alt={image.title} />
+                      </div>
+                    )) || (
+                      <div className='image-container'>
+                        <img
+                          src={image.link}
+                          alt={image.title}
+                          className='image'
+                        />
+                      </div>
+                    )}
+                </div>
+                <p className='image-description'>{image.description}</p>
               </div>
-              <p className='image-description'>{image.description}</p>
-            </div>
-          );
-        })
-      ) : (
-        <div className='image-wrapper'>
-          <img className='image' src={gallery.link} />
-        </div>
-      )}
+            );
+          })
+        ) : (
+          <div className='image-wrapper'>
+            <img className='image' src={gallery.link} />
+          </div>
+        )}
+      </div>
+      {gallery.tags &&
+        gallery.tags.map((tag: string) => (
+          <div className='content-tags'>
+            <span className='tag'>{tag}</span>
+          </div>
+        ))}
       {gallery.description && (
         <p className='description'>{gallery.description}</p>
       )}
